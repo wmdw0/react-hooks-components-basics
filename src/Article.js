57163a22d4551b5fb2d76834e19e3d3529a55ee0
William Walsh <wmdwalsh0@gmail.com>
2021-07-20 21:42:59 -0400

    completed assignment

--- src/Article.js
+++ src/Article.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 function Article() {
-  return (
-    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
-  );
+  return <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>;
 }
 
 export default Article;
